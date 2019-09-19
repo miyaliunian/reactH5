aef3b201ede356cafece087526e6a88062563ebe
fei.wu <fei.wu@neusoft.com>
2019-09-19 10:48:14 +0800

    错误边界处理

--- src/baseUI/ErrorBoundary/ErrorBoundary.js
+++ src/baseUI/ErrorBoundary/ErrorBoundary.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 import './style.less'
 
 export default class ErrorBoundary extends React.Component {
@@ -20,9 +21,17 @@ export default class ErrorBoundary extends React.Component {
     render() {
         if (this.state.hasError) {
             return (
-                <div className="error-wrapper">
-                    <div className="error-title">页面出错了</div>
-                    <a className="error-link" href="/">回到首页</a>
+                <div className="not-match-wrapper">
+                    <div className="img-wrapper">
+                        <img className="bg" src="https://b-gold-cdn.xitu.io/v3/static/img/bg.1f516b3.png" alt=""/>
+                        <img className="panfish" src="https://b-gold-cdn.xitu.io/v3/static/img/panfish.9be67f5.png"
+                             alt=""/>
+                        <img className="sea" src="https://b-gold-cdn.xitu.io/v3/static/img/sea.892cf5d.png" alt=""/>
+                        <img className="spray" src="https://b-gold-cdn.xitu.io/v3/static/img/spray.bc638d2.png" alt=""/>
+                    </div>
+                    <div className="link-wrapper">
+                        <Link to="/" className="link">回到首页</Link>
+                    </div>
                 </div>
             )
         }
