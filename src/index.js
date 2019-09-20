528225844760e339d394cdc9cbc866fb747a7932
fei.wu <fei.wu@neusoft.com>
2019-09-20 09:43:10 +0800

    更新PWA

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.css';
 import '@assets/less/reset.css'
 import '@assets/less/border.less'
 import App from '@baseUI/App/AppContainer';
+import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
     <Provider store={store}>
             <App/>
@@ -13,3 +14,7 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
