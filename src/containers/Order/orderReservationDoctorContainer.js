8d8baff2c0e5537920dba8e10a03f74b1523861f
wufei <Q!w2e3r4t5>
2019-11-25 09:37:59 +0800

    bug

--- src/containers/Order/orderReservationDoctorContainer.js
+++ src/containers/Order/orderReservationDoctorContainer.js
@@ -13,14 +13,15 @@ class OrderReservationDoctorContainer extends Component {
 
 
   render() {
-    return <div />;
+    return <div/>;
   }
 
   componentDidMount() {
     const { history } = this.props;
     this.timer = setTimeout(() => {
       //H5调用取值
-      window["J2C"].regAgain("2123", function(e) {});
+      window["J2C"].regAgain("2123", function(e) {
+      });
       //回传值给H5
       window["J2C"]["regAgainCallBack"] = function(response) {
         let resObj = JSON.parse(response);
@@ -31,7 +32,8 @@ class OrderReservationDoctorContainer extends Component {
         let path = {
           pathname: "/doctor",
           state: {
-            doctorInfo: resObj.params
+            doctorInfo: resObj.params,//医生信息
+            deptInfo: { "name": resObj.params.deptName } // 科室名称
           }
         };
         history.push(path);
@@ -46,26 +48,27 @@ class OrderReservationDoctorContainer extends Component {
   //测试
   getValue() {
     const { history } = this.props;
-    //H5调用取值
-    window["J2C"].regAgain("2123", function(e) {});
-    //回传值给H5
-    window["J2C"]["regAgainCallBack"] = function(response) {
-      let resObj = JSON.parse(response);
-      let token = {};
-      resObj.params.introduction = "";
-      resObj.params.skills = "";
-      token.access_token = resObj.access_token;
-      token.refresh_token = resObj.refresh_token;
-      sessionStorage.setItem("token", JSON.stringify(token));
-      let path = {
-        pathname: "/doctor",
-        state: {
-          doctorInfo: resObj.params
-        }
+    this.timer = setTimeout(() => {
+      //H5调用取值
+      window["J2C"].regAgain("2123", function(e) {
+      });
+      //回传值给H5
+      window["J2C"]["regAgainCallBack"] = function(response) {
+        let resObj = JSON.parse(response);
+        let token = {};
+        token.access_token = resObj.access_token;
+        token.refresh_token = resObj.refresh_token;
+        sessionStorage.setItem("token", JSON.stringify(token));
+        let path = {
+          pathname: "/doctor",
+          state: {
+            doctorInfo: resObj.params,
+            deptInfo: { "name": resObj.params.deptName }
+          }
+        };
+        history.push(path);
       };
-      debugger;
-      history.push(path);
-    };
+    }, 10);
   }
 }
 
