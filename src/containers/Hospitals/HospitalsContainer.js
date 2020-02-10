265eefd91685f30c95cef1759db5e74f6166aae6
wufei <Q!w2e3r4t5>
2020-02-10 17:09:46 +0800

    updateFrame

--- src/containers/Hospitals/HospitalsContainer.js
+++ src/containers/Hospitals/HospitalsContainer.js
@@ -8,36 +8,28 @@
 import React, { Component } from 'react'
 import SafeAreaView from '@baseUI/SafeAreaView/SafeAreaView'
 import Tabs from './components/Tabs/Tabs'
-import HosiContentList from "@containers/Hospitals/components/HosiContentList/HosiContentList";
+import HosiContentList from '@containers/Hospitals/components/HosiContentList/HosiContentList'
 import LoadingMask from '../../components/Loading/LoadingMask'
-
 
 //样式
 import './style.less'
 
 class HospitalsContainer extends Component {
-
-
   render() {
-
     return (
-      <SafeAreaView showBar={true} title={'医院列表'} isRight={true}  handleBack={this.handleBack}>
-        <div className={'hospitalsContainer'} >
+      <SafeAreaView showBar={true} title={'医院列表'} isRight={true} handleBack={this.handleBack}>
+        <div className={'hospitalsContainer'}>
           <Tabs />
-          <HosiContentList/>
+          <HosiContentList />
           <LoadingMask />
         </div>
       </SafeAreaView>
     )
   }
 
-
   handleBack = () => {
     this.props.history.goBack()
   }
-
-
 }
-
 
 export default HospitalsContainer
