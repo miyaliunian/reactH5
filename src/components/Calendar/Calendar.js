6514e3582202e7b568c44fc00e5ca961da6cc104
wufei <Q!w2e3r4t5>
2020-02-10 12:30:15 +0800

    updateFrame

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,15 +1,20 @@
 /**
- * Class: Calendar
+ * Class: 日期选择
  * Author: wufei
  * Date: 2019/6/19
  * Description:
  *
  */
 import React, { Component } from "react";
-import CalendarHeader from "./CalendarHeader";
-import CalendarMain from "./CalendarMain";
-import "./style.less";
 import { getDate } from "@utils/dayutils";
+import NavBar from "@components/NavBar/NavBar";
+// import CalendarHeader from './CalendarHeader'
+// import CalendarMain from './CalendarMain'
+//样式
+import "./Calendar.less";
+import classnames from "classnames";
+import { Icon } from "antd-mobile";
+
 
 const displayDaysPerMonth = year => {
   //定义每个月的天数，如果是闰年第二月改为29天
@@ -46,7 +51,7 @@ const displayDaysPerMonth = year => {
       monthData.unshift([]);
     }
     //添入当前月
-    for (let i = 0; i < daysCount; ) {
+    for (let i = 0; i < daysCount;) {
       monthData.push(++i);
     }
     //补足下一个月
@@ -63,6 +68,174 @@ const getMonths = data => {
   });
   return months;
 };
+
+
+
+ class CalendarHeader extends Component {
+  render() {
+    return (
+      <div className="calendarHeader">
+        <div className="prev" onClick={this.props.prevMonth}>
+          <Icon type="left" />
+        </div>
+        <span className="dateInfo">
+          {" "}
+          {this.props.year}年{this.props.month + 1}月{" "}
+        </span>
+        <div className="next" onClick={this.props.nextMonth}>
+          <Icon type="right" />
+        </div>
+      </div>
+    );
+  }
+}
+
+
+class CalendarMain extends Component {
+  state = {
+    week_names: ["日", "一", "二", "三", "四", "五", "六"]
+  };
+
+  /**
+   * 绑定颜色改变事件
+   */
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="calendar_layout">
+        {this.renderWeekHeader()}
+        {this.renderWeekBody()}
+      </div>
+    );
+  }
+
+  /**
+   * 渲染日历头部
+   * @returns {*}
+   */
+  renderWeekHeader() {
+    return (
+      <div className="calendar_header" ref="header" style={{ height: 45 }}>
+        {this.state.week_names.map((name, index) => {
+          return (
+            <div className="calendar_header_box" key={index}>
+              <div className={"calendar_header_txt"}>{name}</div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  /**
+   * 渲染日历内容
+   * @returns {*}
+   */
+  renderWeekBody() {
+    //作色的日期
+    let fillterMonths = this.props.fillterMonths;
+    //确定当前月数据中每一天所属的月份，以此赋予不同className
+    let month = this.props.viewData[this.props.month],
+      rowsInMonth = [],
+      i = 0;
+    month.forEach((day, index) => {
+      if (index % 7 === 0) {
+        /**
+         *
+         * 数据扁平化,
+         * 将原来的对象 新增2个属性，isCur:标识是否为当前日期、isStatus：标识当前日期能否预约
+         * data:{
+         * day:日期
+         *  isCur: 是否为当期日期
+         *  isStatus: 是否为可预约
+         * }
+         * */
+        let newMonths = [];
+        month.slice(index, index + 7).map(day => {
+          let data = {};
+          data.day = day; // 日期
+          if (new Date().getDate() === day) {
+            data.isCur = true; // 当前日期
+          } else {
+            data.isCur = false; // 当前日期
+          }
+          if (fillterMonths.includes(day)) {
+            data.isStatus = true; // 是否能预约
+          } else {
+            data.isStatus = false;
+          }
+          newMonths.push(data);
+        });
+        rowsInMonth.push(newMonths);
+      }
+
+    });
+    return (
+      <div className={"calendar_body"}>
+        {rowsInMonth.map((row, rowIndex) => {
+          return (
+            <div className={"calendar_body_row"} key={rowIndex}>
+              {row.map(day => {
+                return (
+                  <div
+                    className={"calendar_body_box"}
+                    onClick={e => this.handleDatePick(e, day)}
+                    key={i++}
+                  >
+                    {this.props.isRender === 0 ? (
+                      <div
+                        className={
+                          day.isCur
+                            ? "calendar_body_txt boxCurSel"
+                            : day.isStatus
+                            ? "calendar_body_txt boxSel"
+                            : "calendar_body_txt"
+                        }
+                      >
+                        {day.day}
+                      </div>
+                    ) : (
+                      <div className={"calendar_body_txt"}>{day.day}</div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  /**
+   *  处理日期点击
+   * @param e 虚拟Dom 更改选中的颜色
+   * @param data  点击的日期
+   */
+  handleDatePick(e, day) {
+    if (!Array.isArray(day.day)) {
+      let previousEl = null;
+      previousEl = e.target;
+      previousEl.style = "";
+      let date = new Date(),
+        Y = date.getFullYear() + "-",
+        M =
+          (date.getMonth() + 1 < 10
+            ? "0" + (date.getMonth() + 1)
+            : date.getMonth() + 1) + "-",
+        D = day.day < 10 ? "0" + day.day : day.day;
+      // console.log(Y + M + day.day)
+      // e.target.style = "background:#007FFE;color:#000";
+      this.props.datePick(Y + M + D);
+    }
+  }
+
+}
+
+
 
 export default class Calendar extends Component {
   constructor(props) {
@@ -123,25 +296,25 @@ export default class Calendar extends Component {
       viewData: displayDaysPerMonth(this.state.year),
       fillterMonths: getMonths(this.props.reservations)
     };
+    const { show,onBack } = this.props;
     return (
-      <div className="output">
-        <div className="main" ref="main">
-          <CalendarHeader
-            prevMonth={() => this.prevMonth()}
-            nextMonth={() => this.nextMonth()}
-            year={this.state.year}
-            month={this.state.month}
-            day={this.state.day}
-          />
-          <CalendarMain
-            {...props}
-            datePick={date => this.props.markDate(date)}
-            isRender={this.isCurrentMonth}
-            year={this.state.year}
-            month={this.state.month}
-            day={this.state.day}
-          />
-        </div>
+      <div className={classnames("calendar", { hidden: !show })}>
+        <NavBar onBack={() => onBack()} title={"选择出诊日期"} isShowSearchIcon={false}/>
+        <CalendarHeader
+          prevMonth={() => this.prevMonth()}
+          nextMonth={() => this.nextMonth()}
+          year={this.state.year}
+          month={this.state.month}
+          day={this.state.day}
+        />
+        <CalendarMain
+          {...props}
+          datePick={date => this.props.markDate(date)}
+          isRender={this.isCurrentMonth}
+          year={this.state.year}
+          month={this.state.month}
+          day={this.state.day}
+        />
       </div>
     );
   }
