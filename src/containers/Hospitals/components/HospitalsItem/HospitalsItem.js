7155c45bea4916e944a71a8832fe3f28bff99f14
wufei <Q!w2e3r4t5>
2020-02-10 17:16:01 +0800

    updateFrame

--- src/containers/Hospitals/components/HospitalsItem/HospitalsItem.js
+++ src/containers/Hospitals/components/HospitalsItem/HospitalsItem.js
@@ -1,54 +1,48 @@
-
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { withRouter } from "react-router-dom";
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 //图标
-import icon_sj from "@assets/images/Home/三甲图标IOS.png";
-import icon_bg from "@assets/images/Home/报告图标IOS.png";
-import icon_zh from "@assets/images/Home/综合图标IOS.png";
-import icon_yy from "@assets/images/Home/预约图标IOS.png";
+import icon_sj from '@assets/images/Home/三甲图标IOS.png'
+import icon_bg from '@assets/images/Home/报告图标IOS.png'
+import icon_zh from '@assets/images/Home/综合图标IOS.png'
+import icon_yy from '@assets/images/Home/预约图标IOS.png'
 //样式
-import "./style.less";
-
+import './style.less'
 
 class HospitalsItem extends Component {
   render() {
-    const { itemData } = this.props;
+    const { itemData } = this.props
     return (
-      <Link to={`/division/${itemData.id}/${itemData.name}`} >
+      <Link to={`/division/${itemData.id}/${itemData.name}`}>
         <li className="hospitalsItem__con border-topbottom">
           <div className="hospitalsItem__title">{itemData.name}</div>
           <div className="hospitalsItem__middle">
-            <div className={"hospitalsItem__middle__item"}>
-              <img src={icon_sj} alt="" className={"hospitalsItem__middle__icon"}/>
-              <span className={"hospitalsItem__middle__innerTxt"}>{itemData.hosGradeShortName}</span>
+            <div className={'hospitalsItem__middle__item'}>
+              <img src={icon_sj} alt="" className={'hospitalsItem__middle__icon'} />
+              <span className={'hospitalsItem__middle__innerTxt'}>{itemData.hosGradeShortName}</span>
             </div>
-            <div className={"hospitalsItem__middle__item"}>
-              <img src={icon_zh} alt="" className={"hospitalsItem__middle__icon"}/>
-              <span className={"hospitalsItem__middle__innerTxt"}>{itemData.hosCategory}</span>
+            <div className={'hospitalsItem__middle__item'}>
+              <img src={icon_zh} alt="" className={'hospitalsItem__middle__icon'} />
+              <span className={'hospitalsItem__middle__innerTxt'}>{itemData.hosCategory}</span>
             </div>
             {itemData.regOpened ? (
-              <div className={"hospitalsItem__middle__item"}>
-                <img src={icon_yy} alt="/" className={"hospitalsItem__middle__icon"}/>
-                <span className={"hospitalsItem__middle__innerTxt"}>可预约</span>
+              <div className={'hospitalsItem__middle__item'}>
+                <img src={icon_yy} alt="/" className={'hospitalsItem__middle__icon'} />
+                <span className={'hospitalsItem__middle__innerTxt'}>可预约</span>
               </div>
             ) : null}
             {itemData.reportOpened ? (
-              <div className={"hospitalsItem__middle__item"}>
-                <img src={icon_bg} className={"hospitalsItem__middle__icon"} alt=""/>
-                <span className={"hospitalsItem__middle__innerTxt"}>查报告</span>
+              <div className={'hospitalsItem__middle__item'}>
+                <img src={icon_bg} className={'hospitalsItem__middle__icon'} alt="" />
+                <span className={'hospitalsItem__middle__innerTxt'}>查报告</span>
               </div>
             ) : null}
           </div>
-          <div className="hospitalsItem__address">地址：{itemData.address}</div>
+          <span className="hospitalsItem__address">地址：{itemData.address}</span>
         </li>
       </Link>
-    );
+    )
   }
-
-
-
 }
 
-
-export default withRouter(HospitalsItem);
+export default withRouter(HospitalsItem)
