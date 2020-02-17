190af97521695b3cd386c789fe548d601d37adca
cuiyong <cy20625134@163.com>
2020-02-17 14:10:54 +0800

    门诊缴费详情7

--- src/containers/MyOrder/components/MyRegisterItem/MyRegisterItem.js
+++ src/containers/MyOrder/components/MyRegisterItem/MyRegisterItem.js
@@ -27,7 +27,7 @@ class MyRegisterItem extends Component {
                             <div className={'my-outpatientPayment-item-part1-left-date'}>{this.timeChanger(item.regDate)}</div>
                         </div>
                         <div className={'my-outpatientPayment-item-part1-right'}>
-                            {this.packagePaymentStatusDiv(item.regStatus)}
+                            {this.packagePaymentStatusDiv(item)}
                             {/*<div className={'my-outpatientPayment-item-part1-right-status'}>{item.paymentStatus}</div>*/}
                         </div>
                     </div>
@@ -68,26 +68,72 @@ class MyRegisterItem extends Component {
         return year + "-" + month + "-" + day+" "+hour+":"+minute+":"+second;
     }
 
-    packagePaymentStatusDiv(status){
-        if(status==0){
-            return (<div className={'my-outpatientPayment-item-part1-right-status-blue'}>
-                待支付
-            </div>);
-        }else{
-            return (<div className={'my-outpatientPayment-item-part1-right-status'}>
-                {this.paymentStatus2Str(status)}
-            </div>);
+    packagePaymentStatusDiv(item){
+        let statusLabel='';
+        let divClassName='my-outpatientPayment-item-part1-right-status';
+        switch (item.regStatus) {
+            case 0:
+                //线下支付
+                if (item.paymentMethod == 0) {
+                    divClassName='my-outpatientPayment-item-part1-right-status-blue';
+                    statusLabel = '预约成功'
+                } else {
+                    //支付状态为：0未支付、1部分支付；显示支付按钮
+                    switch (item.paymentStatus) {
+                        case 0:
+                        case 1:
+                            statusLabel = '待支付';
+                            divClassName='my-outpatientPayment-item-part1-right-status-blue';
+                            break
+                        case 2:
+                            statusLabel = '支付确认中';
+                            break
+                        case 3:
+                            statusLabel = '已支付';
+                            divClassName='my-outpatientPayment-item-part1-right-status-blue';
+                            break
+                        case 4://部分退款
+                        case 5://已退款待确认(第三方发送退费申请)
+                            statusLabel = '退款中'
+                            break
+                        case 6:
+                            statusLabel = "已退款"
+                            break
+                    }
+                }
+                break
+            case 1:
+                statusLabel = '就诊完成'
+                break
+            case 2:
+                //取消
+                statusLabel = '已取消预约'
+                break
+            default:
+                statusLabel = '预约超期'
+                break
         }
+        return (<div className={divClassName}>
+            {statusLabel}
+        </div>);
     }
 
     paymentStatus2Str(status) {
         switch (status) {
             case 0:
-                return '待支付';
+                return '未支付';
             case 1:
-                return '已支付';
+                return '部分支付';
             case 2:
-                return '已取消预约';
+                return '已支付';
+            case 3:
+                return '已支付';
+            case 4:
+                return '部分退款';
+            case 5:
+                return '已退款';
+            case 6:
+                return '已退款';
             default:
                 return '未知';
         }
