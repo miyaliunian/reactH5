2f9e60b9c081af4579c5788c65d448647ceb7a45
米亚流年 <354849262@qq.com>
2020-04-04 11:32:03 +0800

    updateFrame

--- src/components/Loading/LoadingMask.js
+++ src/components/Loading/LoadingMask.js
@@ -26,8 +26,7 @@ export default class LoadingMask extends Component {
     return (
       <div className="loading" id={"loadingMask"}>
         <div className={"loading__contain"}>
-          <ReactLoading type={"spinningBubbles"} width={40} height={40} />
-          <span className={"loading__txt"}>正在加载 ...</span>
+          <ReactLoading type={"spinningBubbles"} width={55} height={55} color={'#0084ff'}/>
         </div>
       </div>
     );
